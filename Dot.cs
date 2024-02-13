146cbebd40aafae99e567b36666b1dd0a966574d
MegaHamer <135736897+MegaHamer@users.noreply.github.com>
2024-02-13 14:32:29 +0300

    Добавьте файлы проекта.

---
+++ Dot.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingDots
+{
+    
+    internal class Dot
+    {
+        public class Point
+        {
+            public double x = 0;
+            public double y = 0;
+            public Point(int x,int y)
+            {
+                this.x = (double)x;
+                this.y = (double)y;
+            }
+            public Point(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public Point()
+            {
+                this.x = 0;
+                this.y = 0;
+            }
+            public void RandPos(Random r,int x1,int y1,int x2,int y2)
+            {
+                //Random r = new Random();
+                x= r.Next(x1,x2);
+                y= r.Next(y1,y2);
+            }
+            public void RandVec(Random r,double spead = 1 )
+            {
+                //Random r =new Random();
+
+                int x1 = r.Next(-10,10);
+                int y1 = r.Next(-10,10);
+
+                double len = Math.Sqrt(x1*x1+y1*y1);
+
+                double x2 = x1/len;
+                double y2 = y1/len;
+                
+                this.x = spead* x2;
+                this.y = spead* y2;
+            }
+        }
+        public Point Position;
+        public Point Vector;
+        public Point FirstDot;
+        public Point SecondDot;
+        public Dot(Random r,int x1,int y1,int x2,int y2, bool pos = false, bool vec = false,int spead = 100)
+        {
+            FirstDot = new Point(Math.Min(x1, x2), Math.Min(y1, y2));
+            SecondDot = new Point(Math.Max(x1, x2), Math.Max(y1, y2));
+
+            Position = new Point();
+            Vector = new Point();
+
+            if (pos)
+            {
+                Position = new Point();
+                Position.RandPos(r,((int)FirstDot.x),((int)FirstDot.y), ((int)SecondDot.x), ((int)SecondDot.y));
+            }
+            if (vec)
+            {
+                Vector= new Point();
+                Vector.RandVec(r,spead/100);
+            }
+        }
+        public void Step()
+        {
+            if (FirstDot.x<Position.x+Vector.x && Position.x + Vector.x < SecondDot.x)
+            { 
+                Position.x += Vector.x;
+            }
+            else
+            {
+
+            }
+            if (FirstDot.y < Position.y + Vector.y && Position.y + Vector.y < SecondDot.y)
+            {
+                Position.y += Vector.y;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingDots
+{
+    public partial class Form1 : Form
+    {
+        List<Dot> dots = new List<Dot> { };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateNewDots();
+        }
+        private void GenerateNewDots()
+        {
+            Random r = new Random();
+            dots.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                dots.Add(new Dot(r, 0,0,this.Width,this.Height,true,true,100));
+            }
+        }
+        private void draw_points()
+        {
+            Bitmap Image = new Bitmap(this.Width, this.Height);
+            Graphics gr = Graphics.FromImage(Image);
+            gr.FillRectangle(Brushes.White, 0, 0, Width, Height);
+            Pen pn = new Pen(Color.Red, 1);
+
+
+            foreach (Dot d in dots)
+            {
+                gr.DrawEllipse(pn, (float) d.Position.x,(float) d.Position.y, 10, 10);
+            }
+            var FormG = CreateGraphics();
+            FormG.DrawImageUnscaled(Image, 0, 0);
+        }
+        private void clear_scr()
+        {
+            Graphics gr = this.CreateGraphics();
+            //gr.Clear(SystemColors.Control);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            draw_points();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clear_scr();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (Dot d in dots)
+            {
+                d.Step();
+            }
+            clear_scr();
+            draw_points();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button3_Click(new object(), new EventArgs());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                timer1.Start();
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            GenerateNewDots();
+        }
+    }
+}
